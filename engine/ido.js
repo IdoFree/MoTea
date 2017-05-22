f5abb3d02c1c531d4f18c90d913d4c48901139d7
IdoFree <306634799@qq.com>
2017-05-22 15:24:44 +0800

    Add files via upload

--- engine/ido.js
+++ engine/ido.js
@@ -1,24 +1,50 @@
-/**
- * Created by Administrator on 2017-05-21.
- */
 var fs=require("fs");
 
 //<%foreach var item = ${result}%>
-var foreach_patt = /<%foreach\s+var\s+item\s*=\s*\$\{([a-zA-Z]+)\}%>/g;
-
+var foreach_patt = /<%foreach\s+var\s+item\s*=\s*\$\{([a-zA-Z]+)\}%>([\s\S]*)<%\/foreach%>/g;
+var attr_patt = /\$\{\w[a-zA-Z0-9]*\}/g;
 exports.Render = Render;
 
-function Render(response){
+function Render(response,source){
     this.response = response ;
+	
+	
+	
     var render = function(fname,options){
         fs.readFile(fname,function(err,data ){
             if(err) throw  err;
+			data = data.toString()
+			var matchRst = foreach_patt.exec(data);
+			if(matchRst && matchRst.length > 1){
+				//get the result set , and continue to retrieve the left String 
+				var loopSetName = matchRst[1];
+				var loopSet = options[loopSetName];
+				var loopString = matchRst[2];
+				var attrs = attr_patt.exec(loopString);
+				if(attrs){
+					//get the attr , and replace it from the resultSet;
+					for(var i = 0; i < attrs.length ; i++){
+						var attrVal = loopSet[attrs[i]]
+						//replace the ${xx}
+						data = replace_attr(data,attrs[i],attrVal);
+					}
+				}
+				
+				 
+			}
+			
 
         });
-        this.response.write();
+        this.response.write(data);
 
         this.response.end();
     }
+	
+	function replace_attr(data,attr_name,attr_val){
+		var specific_attr_patt = "\$\{\s*"+attr_name+"\s*\}";
+		specific_attr_patt = new RegExp(specific_attr_patt,"g");
+		return data.replace(attr_patt,attr_val);
+	}
 
 }
 
